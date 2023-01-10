950eca4623f5cb6bdff65de4c47f269cf27d6ca3
Michael Testut <michael.testut@yahoo.com>
2023-01-09 22:55:35 -0800

    started perform request method

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -7,15 +7,41 @@
 //
 
 import Foundation
+protocol CoinManagerDelegate{
+    func didUpdatePrice(_ coinManager:CoinManager)
+    func didFailWithError(error:Error)
+}
 
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
     let apiKey = "YOUR_API_KEY_HERE"
     
+    var delegate: CoinManagerDelegate?
+    
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-
+    
     func getCoinPrice(for currency:String){
         //finish function
     }
+    
+    func performRequest(with urlString:String){
+        if let url = URL(string: urlString){
+            let session = URLSession(configuration:  .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil{
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+                if let safeData = data{
+                    if let price = self.parseJSON(safeData){
+                        self.delegate?.didUpdatePrice(self, price: price)
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    
 }
